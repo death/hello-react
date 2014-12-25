6694ef0d2d74b297be94167dce609c09f8d57e42
death <death@nessers.org>
2014-12-25 20:07:25 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var React = require("react");
+var ReactBootstrap = require("react-bootstrap");
+var _ = require("underscore");
+var superagent = require("superagent");
+
+var App = React.createClass({
+    render: function() {
+        return (
+                <h1>Hello, React</h1>
+        );
+    }
+});
+
+React.render(<App />, document.body);
