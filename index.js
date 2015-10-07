85239f80e4b73ca65bc3e2f4af9e3e87c76430c3
death <death@nessers.org>
2015-10-07 22:22:38 +0300

    react 0.14, webpack

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require("./style/main.less");
+
 var React = require("react");
+var ReactDOM = require("react-dom");
 var ReactBootstrap = require("react-bootstrap");
 var _ = require("lodash");
 var superagent = require("superagent");
@@ -11,4 +14,4 @@ var App = React.createClass({
     }
 });
 
-React.render(<App />, document.body);
+ReactDOM.render(<App />, window.root);
