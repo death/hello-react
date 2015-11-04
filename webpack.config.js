d177f7b2c9e9c92cdf63dfbc6f099793cfd7d27a
death <death@nessers.org>
2015-11-04 23:48:02 +0200

    specify output publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ module.exports = {
     entry: "./index.js",
     output: {
         path: "static/gen/",
-        filename: "bundle.js"
+        filename: "bundle.js",
+        publicPath: "gen/"
     },
     module: {
         loaders: [
